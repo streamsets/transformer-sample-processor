3d4f95f1166416b78a2608217f324e651a1f92b4
Pat Patterson <metadaddy@gmail.com>
2019-04-04 20:15:28 -0700

    Lookup credit card type from credit card number

--- src/main/scala/com/example/processor/sample/SampleProcessor.scala
+++ src/main/scala/com/example/processor/sample/SampleProcessor.scala
@@ -2,17 +2,30 @@ package com.example.processor.sample
 
 import java.util
 
-import com.streamsets.datatransformer.api.spark.SingleInputSparkTransform
+import com.streamsets.datatransformer.api.spark.{SingleInputSparkTransform, SparkSessionProvider}
 import com.streamsets.pipeline.api.ConfigIssue
 import com.streamsets.pipeline.spark.SparkData
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{broadcast, explode, split}
 
 /** Sample processor
   *
   *  @constructor create a new processor
   */
 class SampleProcessor extends SingleInputSparkTransform {
+  private val ccTypes = List(
+    ("Visa", "4"),
+    ("Mastercard", "51,52,53,54"),
+    ("AMEX", "34,37"),
+    ("Diners Club", "300,301,302,303,304,305,36,38"),
+    ("Discover", "6011,65"),
+    ("JCB", "2131,1800,35")
+  )
+  private var ccTypeDF: DataFrame = null
+
   /**
-    * Initializes the processor.
+    * Initializes the transformer, converting the a static list of credit card type/prefix mappings into a
+    * [[org.apache.spark.sql.DataFrame]].
     *
     * @return a List of any [[com.streamsets.pipeline.api.ConfigIssue]]s found by the super class constructor
     */
@@ -20,14 +33,20 @@ class SampleProcessor extends SingleInputSparkTransform {
     val issues = super.init()
 
     if (issues.size() == 0) {
-      // Perform any initialization
+      val sparkSession = SparkSessionProvider.spark()
+      import sparkSession.implicits._
+
+      // Make a DataFrame suitable for joining with the input data
+      ccTypeDF = ccTypes.toDF("type", "prefixes")
+          .withColumn("prefix", explode(split($"prefixes",",")))
+          .drop("prefixes")
     }
 
     issues
   }
 
   /**
-    * Transforms the input [[com.streamsets.pipeline.spark.SparkData]] into the
+    * Looks up the credit card numbers in the input [[com.streamsets.pipeline.spark.SparkData]] and sets them in the
     * output.
     *
     * @param input [[com.streamsets.pipeline.spark.SparkData]] containing a [[org.apache.spark.sql.DataFrame]]
@@ -39,10 +58,16 @@ class SampleProcessor extends SingleInputSparkTransform {
     // Short circuit if no incoming data
     if (df.count() == 0) return input
 
-    // Apply required operations on the DataFrame before returning it in a
-    // new SparkData object
+    val sparkSession = SparkSessionProvider.spark()
+    import sparkSession.implicits._
+
+    // Join the input data with the credit card types DataFrame where the incoming credit card number starts with
+    // a prefix
     new SparkData(
       df
+          .join(broadcast(ccTypeDF), $"credit_card".startsWith($"prefix"),"left_outer")
+          .drop("prefix")
+          .withColumnRenamed("type", "credit_card_type")
     )
   }
 }
